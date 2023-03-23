42ffc2663e57410a2858dc5651ab99e6c9ac05a8
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-03-23 08:47:48 -0300

    models and dbcontext data

---
+++ Models/Driver.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+  public class Driver
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DriveNb { get; set; }
+  }
+}
