5a214acfcd72e50ed5d2ba676e4f370520c9ad31
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-03-23 08:55:50 -0300

    caching services

---
+++ Services/CacheService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace TodoApi.Services
+{
+  public class CacheService : ICacheService
+  {
+    IDatabase _cacheDb;
+
+    public CacheService()
+    {
+      var redis = ConnectionMultiplexer.Connect("localhost:6379");
+      _cacheDb = redis.GetDatabase();
+    }
+    public T GetData<T>(string key)
+    {
+      var value = _cacheDb.StringGet(key);
+      if (!String.IsNullOrWhiteSpace(value)) return JsonSerializer.Deserialize<T>(value);
+
+      return default;
+    }
+
+    public object RemoveData(string key)
+    {
+      var _exist = _cacheDb.KeyExists(key);
+      if (_exist) return _cacheDb.KeyDelete(key);
+
+      return false;
+    }
+
+    public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+      var expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+      var isSet = _cacheDb.StringSet(key, JsonSerializer.Serialize(value), expiryTime);
+
+      return isSet;
+    }
+  }
+}
